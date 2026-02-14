bb9fe378f92b9c7dcaffe9ae468576a1e7126107
Dhananjay Surti <djsurti3003@gmail.com>
2026-02-14 14:46:15 -0800

    Connect frontend with backend api

--- service/api/app/main.py
+++ service/api/app/main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware 
 from .routes import images
 import uvicorn
+
 origins = [
-    "http://localhost:8080"
+    "http://localhost:8081",
+    "http://localhost:19006",
+    "http://localhost:8080",
+    "http://10.250.81.211:8081",
+    "http://10.250.81.211:19006",
+    "*",  # Allow all origins in development
 ]
 
 app = FastAPI()
